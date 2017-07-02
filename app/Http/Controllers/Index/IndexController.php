4a74fada0457e5293aecc11cadec231ad9b7b1b6
Leeon <sviplee@gmail.com>
2017-07-02 18:04:03 +0800

    update config

---
+++ app/Http/Controllers/Index/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Index;
+
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function __construct()
+    {/*{{{*/
+         
+    }/*}}}*/
+
+    public function showIndex()
+    {/*{{{*/
+        dd('Hello World !');
+    }/*}}}*/
+}
+
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'Index', 'prefix' => 'index'], function () {
+    Route::get('/', 'IndexController@showIndex');
+});
